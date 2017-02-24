efb44b9f9e60227c0ffe3e1ecadcff82dd3fc14f
alma31 <lenni31@neuf.fr>
2017-02-24 19:56:58 +0100

    init commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nom');
+            $table->string('prenom');
+            $table->integer('age');
+            $table->integer('cp');
+            $table->string('ville');
+            $table->string('email')->unique();
+            $table->integer('tel');
+            $table->integer('nht');
+            $table->integer('nhr');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
