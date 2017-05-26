a663f59d5b64e7aaa3cc5cf2d6457de8fbc653f4
lacombe <alma31>
2017-05-26 19:37:15 +0200

    sr

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,15 +14,15 @@ class CreateUsersTable extends Migration
      public function up()
      {
        Schema::create('users', function (Blueprint $table) {
-           $table->increments('id');
-           $table->string('name');
-           $table->string('email')->unique();
-           $table->string('password');
-           $table->rememberToken();
-           $table->timestamps();
+         $table->increments('id');
+         $table->string('name');
+         $table->string('email');
+         $table->string('password');
+         $table->rememberToken();
+         $table->timestamps();
        });
-   }
-   
+     }
+     
      /**
       * Reverse the migrations.
       *
@@ -31,5 +31,5 @@ class CreateUsersTable extends Migration
      public function down()
      {
        Schema::dropIfExists('users');
+     }
    }
-}
